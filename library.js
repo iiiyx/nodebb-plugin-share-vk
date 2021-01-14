950a39346e1327f0811aba2e9f4fd7c95af5ced0
Antosik <grig_anton96@mail.ru>
2021-01-14 20:59:45 +0300

    feat: remove unnecessary things

--- library.js
+++ library.js
@@ -1,27 +1,19 @@
-(function(module) {
-  'use strict';
-  /* globals module, require */
+'use strict';
 
-  var constants = Object.freeze({
-    'name': 'VkShare',
-    'admin': {
-      'route': '/plugins/share-vkontakte',
-      'icon': 'vk fa-vk'
-    }
+
+var VkShare = {};
+
+VkShare.addNetwork = function (networks, cb) {
+
+  networks.push({
+    id: "vkontakte",
+    name: "Vkontakte",
+    class: "fa-vk"
   });
 
-  var VkShare = {
-    settings: undefined
-  };
-  
-  VkShare.addNetwork = function (networks, cb) {
-    networks.push({
-      id: "vkontakte",
-      name: "Vkontakte",
-      class: "fa-vk"
-    });
-    if (cb && typeof cb === 'function')
-      cb(null, networks);
-  };
-  module.exports = VkShare;
-}(module));
+  if (cb && typeof cb === 'function') {
+    cb(null, networks);
+  }
+};
+
+module.exports = VkShare;
